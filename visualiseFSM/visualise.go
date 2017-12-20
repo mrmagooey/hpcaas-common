33664166ae44285a70c34ffb7ee4680988fed5e6
mrmagooey <mrmagooey@users.noreply.github.com>
2017-12-20 14:33:58 +1100

    add vendoring, add fsm visualisation

---
+++ visualiseFSM/visualise.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mrmagooey/fsmGraphviz"
+	common "github.com/mrmagooey/hpcaas-common"
+)
+
+//go:generate /bin/bash -c "go build && ./visualiseFSM | dot -Tpng -Gdpi=300 -o daemonFsm.png"
+
+func main() {
+	graphVizString, _ := fsmGraphviz.CreateGraphvizString(common.Initializing.String(), common.DaemonFSM, "Daemon State")
+	fmt.Println(graphVizString)
+}
